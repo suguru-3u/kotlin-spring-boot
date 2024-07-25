f6a6b6f62a24f9433cc3ffea60a2ee443353c14d
suguru-3u <suguru.13579515@gmail.com>
2024-07-25 16:46:41 +0900

    study kotlin spring boot 7/25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+//	id("com.arenagod.gradle.MybatisGenerator") version "1.4"
+	id("com.thinkimi.gradle.MybatisGenerator") version "2.4"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
-}
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
 }
+
+//java {
+//	toolchain {
+//		languageVersion = JavaLanguageVersion.of(17)
+//	}
+//}
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
@@ -26,14 +31,28 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	// Mybatisで追加
+	implementation("org.mybatis:mybatis:3.5.6")
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
+	implementation("mysql:mysql-connector-java:8.0.28")
+	mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
 }
 
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
 	}
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+mybatisGenerator {
+	verbose = true
+	configFile = "/Users/oohirasuguru/Documents/Kotlin/study-kotlin-spring/demo/src/main/resources/generatorConfig.xml"
+}
+
+
+
